60b77b8a0d8e28c0053a96b406abae2d7d89a80a
phani5757 <phanis900@gmail.com>
2023-03-25 16:59:51 +0530

    Deployment changes.

--- backend/api/index.js
+++ backend/api/index.js
@@ -7,6 +7,7 @@ const userRouter = require("../routes/userRouter");
 const chatRouter = require("../routes/chatRouter");
 const messageRouter = require("../routes/messageRouter");
 const { notFound, errorHandler } = require("../middleware/errorMiddleware");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
@@ -22,6 +23,20 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRouter);
 app.use("/api/chat", chatRouter);
 app.use("/api/message", messageRouter);
+
+const __dirName = path.resolve();
+
+if (process.env.ENVIRONMENT === "production") {
+  app.use(express.static(path.join(__dirName, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirName, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
